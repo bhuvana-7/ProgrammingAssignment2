9c30a2bd23877a2bef51c2f99a4d4358029ff208
Bhuvana Jayaraman <bhuvani.jayaraman@gmail.com>
2024-02-16 23:55:36 -0500

    Adding comments to the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function 
+## set the value of the vector, 
+## get the value of the vector, 
+## set the value of the inverse matrix and 
+## get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+  inv <- NULL 
   set <- function(y) {
     x <<- y
     inv <<- NULL
@@ -18,7 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##cache Solve gets the inverse matrix value and checks if it is null 
+## if it is null, it gets the passed matrix and create inverse of it 
+## assign it to the inverse variable, and
+## set the value to the cache using setInverse function
 
 cacheSolve <- function(x) {
   inv <- x$getInverse()
